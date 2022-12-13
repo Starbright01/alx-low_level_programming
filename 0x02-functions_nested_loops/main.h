6d05e6b44f25bfb9e3f96beeb5b51fc5a6fa04af
starbright01 <ayinlaisreal27@gmail.com>
2022-12-13 00:18:00 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabets_x10(void);
+int_islower(int c);
+int_isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
